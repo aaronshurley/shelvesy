37c02dd693ee546a8299356c81339b1c4506e7bf
Aaron Hurley <aaronshurley@gmail.com>
2014-12-02 21:36:10 -0800

    fixed ShelvedBook validation further

--- app/models/shelved_book.rb
+++ app/models/shelved_book.rb
@@ -14,6 +14,7 @@
 class ShelvedBook < ActiveRecord::Base
   validates :book_id, :shelf_id, :date_added, presence: true
   validate :user_only_has_one_copy_of_book, on: :create
+  before_validation :ensure_date_added
 
   belongs_to :shelf
   has_one :user, through: :shelf
@@ -23,5 +24,9 @@ class ShelvedBook < ActiveRecord::Base
     if user.books.where(id: self.book_id).exists?
       errors.add(:book_id, "already shelved")
     end
+  end
+
+  def ensure_date_added
+    self.date_added ||= DateTime.now
   end
 end
